60d357c32e051914e3fff1d591dfbb66d29094f0
Valeriy Zablodskiy <vzablodskiy1999@gmail.com>
2020-08-18 17:02:24 +0300

    Updates

---
+++ precache-manifest.09a1ae34c5c714b4fcb4fb7baea17699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fb2d6502df7ee23d6460937ea66294",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "c257a0bfdbf410af1aa4",
+    "url": "/currency-converter/static/css/main.e0cdfc47.chunk.css"
+  },
+  {
+    "revision": "2f0948db40fc63af564b",
+    "url": "/currency-converter/static/js/2.94d4263c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/currency-converter/static/js/2.94d4263c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c257a0bfdbf410af1aa4",
+    "url": "/currency-converter/static/js/main.8bd63913.chunk.js"
+  },
+  {
+    "revision": "01bad8a5c927a65d907c",
+    "url": "/currency-converter/static/js/runtime-main.6a747c36.js"
+  }
+]);
